6f7a03eab66692c720422667791d72ed652454b9
success1308 <success.ambaliya13@gmail.com>
2024-06-05 18:55:53 -0400

    Final Library

---
+++ Library/script.js
@@ -0,0 +1,187 @@
+
+
+ if(!localStorage.getItem('storedarr')){
+    localStorage.setItem('storedarr', JSON.stringify([]));
+  }
+
+  // Retrieve stored data
+  const storedData = localStorage.getItem('storedarr');
+  //define new array for empty if no previous data
+  const Book = JSON.parse(storedData);
+
+  displayBook(Book);
+
+  function createBook(title,author,year,bookStatus){
+    return{
+      title:title,
+      author:author,
+      year:year,
+      bookStatus:bookStatus,
+    }
+  }
+
+  function addBookToArr(event){
+    event.preventDefault();
+
+    if (!bookForm.checkValidity()) {
+      bookForm.reportValidity();
+      return;
+    }
+    const bookName = document.querySelector("#book").value;
+    const authorName = document.querySelector("#author").value;
+    const yearBook = document.querySelector("#year").value;
+    const bookStatus = document.querySelector("#Book-status").value;
+    const newBook = createBook(bookName, authorName, yearBook, bookStatus);
+    Book.push(newBook);
+    storeData(Book);
+    bookForm.reset();
+    bookForm.classList.add('hide');
+    displayBook(Book);
+  }
+
+  function displayBook(Book){
+    const bookList = document.querySelector(".main");
+    bookList.innerHTML= '';
+    const newForm = document.createElement('form');
+    newForm.classList.add('sform');
+    newForm.classList.add('hide');
+    newForm.setAttribute('id','bookForm');
+    newForm.innerHTML = `      
+    <label for="book"> Book : </label>
+    <input type="text" id="book" name="book" required > 
+
+    <label for="author"> Author : </label>
+    <input type="text" id="author" name="author" required >
+    
+    <label  for="year"> Year : </label>
+    <input type="number" id="year" name="year" required min="1800" max="2024" >
+
+    <select name="Book-status" id="Book-status" required >
+        <option value="Book-status" selected disabled hidden>Book-status</option>
+        <option value="To-Read">To Read</option>
+        <option value="Reading">Reading</option>
+        <option value="Finished">Finished</option>
+    </select>
+
+    <button id="submit" type="submit">Submit</button>`;
+    bookList.appendChild(newForm);
+
+    const addBook = document.querySelector('.add-book');
+    const bookForm = document.querySelector('.sform');
+  
+  
+    addBook.addEventListener('click', () =>{
+      bookForm.classList.remove('hide');
+      const submitForm = document.querySelector('#submit');
+      submitForm.addEventListener('click', addBookToArr );
+    });
+
+    Book.forEach((book, index) => {
+
+      const bookCard = document.createElement('div');
+      bookCard.classList.add('card');
+
+      const bookTitle = document.createElement('h1');
+      bookTitle.textContent = `Title : ${book.title}`;
+
+      const bookAuthor = document.createElement('p');
+      bookAuthor.textContent = `Author : ${book.author}`;
+
+      const bookYear = document.createElement('p');
+      bookYear.textContent = `Year : ${book.year}`;
+
+
+      const bookSelect = document.createElement('select');
+      ['Book-status','To-Read','Reading','Finished'].forEach((status) => {
+        const bookOption = document.createElement('option');
+        bookOption.value = status;    
+        bookOption.textContent = status;
+
+        if (status === book.bookStatus) {
+          bookOption.selected = true;
+        }
+        bookSelect.appendChild(bookOption);
+      });
+      
+      bookSelect.addEventListener('change', function() {
+        book.bookStatus = this.value; 
+        storeData(Book); 
+      });
+
+      const removeBtn = document.createElement('i');
+      removeBtn.classList.add('fa-solid');
+      removeBtn.classList.add('fa-xmark');
+
+      removeBtn.addEventListener('click', (index) => {
+        Book.splice(index, 1);
+        storeData(Book);
+        displayBook(Book);
+      });
+
+
+      
+      bookCard.appendChild(bookTitle);
+      bookCard.appendChild(bookAuthor);
+      bookCard.appendChild(bookYear);
+      bookCard.appendChild(bookSelect);
+      bookCard.appendChild(removeBtn);
+
+      
+      bookList.appendChild(bookCard);
+
+    });
+
+  }
+
+
+
+  // store data
+  function storeData(booksToStore) {
+    localStorage.setItem('storedarr', JSON.stringify(booksToStore));
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// steps
+
+/* interval timer */
+const
+  timer = 10000,
+  ui = document.getElementById('ui');
+
+setInterval(function() {
+  ui.classList.toggle('switch');
+}, timer);
+
---
+++ code PlayGround/demos.js
@@ -0,0 +1,172 @@
+if(!localStorage.getItem('storedarr')){
+	localStorage.setItem('storedarr', JSON.stringify([]));
+}
+
+// Retrieve stored data
+const storedData = localStorage.getItem('storedarr');
+//define new array for empty if no previous data
+const bookArr = JSON.parse(storedData);
+
+
+// Display books on page load
+displayBook(bookArr);
+
+function createBook(title,author,page,year,BookStatus){
+	return {
+		title : title,
+		author : author,
+		page : page,
+		year : year,
+		BookStatus: BookStatus,
+	}
+}
+
+const bookName = document.querySelector('#book');
+const authorName = document.querySelector('#author');
+const pages = document.querySelector('#pages');
+const yeardata = document.querySelector('#year');
+const bookStatusdata = document.querySelector('#Book-status');
+const submitBtn = document.querySelector('#submit');
+const bookForm = document.querySelector('#bookForm');
+
+
+bookForm.addEventListener('submit', addBook);
+
+
+function addBook(event){
+
+	event.preventDefault();
+
+	if (!bookForm.checkValidity()) {
+		bookForm.reportValidity();
+		return;
+	}
+
+	const book = bookName.value;
+	const author = authorName.value;
+	const page = pages.value;
+	const year = yeardata.value;
+	const BookStatus = bookStatusdata.value;
+	const newBook = createBook(book,author, page, year, BookStatus);
+
+	bookArr.push(newBook);
+	displayBook(bookArr);
+	storeData(bookArr);
+	bookForm.reset();
+} 
+
+
+function displayBook(bookArr){
+
+	const bookList = document.querySelector('.book-list');
+	bookList.innerHTML = '';
+
+	bookArr.forEach( (book, index) => {
+		const bookCard = document.createElement('div');
+		bookCard.classList.add('card');
+		
+		const bookTitle = document.createElement('h2');
+		bookTitle.textContent = `Title : ${book.title}`;
+		
+		const authorName = document.createElement('p');
+		authorName.textContent = `Author : ${book.author}`;
+
+		const pageNo = document.createElement('p');
+		pageNo.textContent = `Pages : ${book.page}`;
+
+		const bookYear = document.createElement('p');
+		bookYear.textContent = `Year : ${book.year}`;
+
+		const bookStatusa = document.createElement('select');
+		bookStatusa.classList.add('select-sec');
+		['To-Read', 'Reading', 'Finished'].forEach(status => {
+			const option = document.createElement('option');
+			option.value = status;
+			option.textContent = status;
+			if (status === book.BookStatus) {
+				option.selected = true;
+			}
+			bookStatusa.appendChild(option);
+		});
+
+		bookStatusa.addEventListener('change', function() {
+			book.BookStatus = this.value; 
+			storeData(bookArr); 
+		});
+
+		const removebtn = document.createElement('button');
+		removebtn.textContent = "Remove";
+		removebtn.addEventListener('click', () => {
+			bookArr.splice(index, 1);
+			storeData(bookArr);
+			displayBook(bookArr);
+		});
+		
+		const btndiv = document.createElement('div');
+		btndiv.classList.add('btns');
+		
+		btndiv.appendChild(bookStatusa);
+		btndiv.appendChild(removebtn);
+
+
+		bookCard.appendChild(bookTitle);
+		bookCard.appendChild(authorName);
+		bookCard.appendChild(pageNo);
+		bookCard.appendChild(bookYear);
+		bookCard.appendChild(btndiv);
+
+		bookList.appendChild(bookCard);
+
+
+
+		setTimeout(() => {
+            bookCard.classList.add('appear');
+        }, 10);
+
+	});
+	
+}
+
+
+// year filter
+const yearAsc = document.querySelector('.yearAsc');
+yearAsc.addEventListener('click', yearFilter);
+function yearFilter(){
+	const sortedBooks = [...bookArr].sort((a, b) => a.year - b.year);
+	displayBook(sortedBooks);
+}
+
+
+// page filter
+const pagesAsc = document.querySelector('.pagesAsc');
+pagesAsc.addEventListener('click', pageFilter);
+function pageFilter(){
+	const sortedBooks1 = [...bookArr].sort((a, b) => a.page - b.page);
+	displayBook(sortedBooks1);
+}
+
+// title filter
+
+const titleAsc = document.querySelector('.titleAsc');
+titleAsc.addEventListener('click', titleFilter);
+function titleFilter(){
+	const sortedBooks = [...bookArr].sort((a, b) => a.title.localeCompare(b.title));
+	displayBook(sortedBooks);
+}
+
+
+// Authr filter
+
+const authorAsc = document.querySelector('.authorAsc');
+authorAsc.addEventListener('click', authorFilter);
+function authorFilter(){
+	const sortedBooks = [...bookArr].sort((a, b) => a.author.localeCompare(b.author));
+	displayBook(sortedBooks);
+}
+
+
+// Storing the bookArr data in localStorage
+
+function storeData(booksToStore) {
+	localStorage.setItem('storedarr', JSON.stringify(booksToStore));
+}
