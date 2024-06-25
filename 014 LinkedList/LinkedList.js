e18277bd8b49a5882215647e17fa2b5850e03106
success1308 <success.ambaliya13@gmail.com>
2024-06-24 23:02:48 -0400

    LinkedList

--- 014 LinkedList/LinkedList.js
+++ 014 LinkedList/LinkedList.js
@@ -1,5 +1,5 @@
 // LinkedList.js
-import { Node } from "./Node";
+import { Node } from "../Node.js";
 
 export class LinkedList {
   constructor() {
@@ -7,6 +7,7 @@ export class LinkedList {
   }
 
   append(value) {
+    // Time complexity: O(n)
     const newNode = new Node(value);
     if (!this.headNode) {
       this.headNode = newNode;
@@ -20,11 +21,13 @@ export class LinkedList {
   }
 
   prepend(value) {
+    // Time complexity: O(1)
     const newNode = new Node(value, this.headNode);
     this.headNode = newNode;
   }
 
   size() {
+    // Time complexity: O(n)
     let count = 0;
     let current = this.headNode;
     while (current) {
@@ -35,10 +38,12 @@ export class LinkedList {
   }
 
   head() {
+    // Time complexity: O(1)
     return this.headNode;
   }
 
   tail() {
+    // Time complexity: O(n)
     if (!this.headNode) return null;
     let current = this.headNode;
     while (current.nextNode) {
@@ -48,6 +53,7 @@ export class LinkedList {
   }
 
   at(index) {
+    // Time complexity: O(n)
     let current = this.headNode;
     let count = 0;
     while (current) {
@@ -59,6 +65,7 @@ export class LinkedList {
   }
 
   pop() {
+    // Time complexity: O(n)
     if (!this.headNode) return null;
     if (!this.headNode.nextNode) {
       const poppedNode = this.headNode;
@@ -75,6 +82,7 @@ export class LinkedList {
   }
 
   contains(value) {
+    // Time complexity: O(n)
     let current = this.headNode;
     while (current) {
       if (current.value === value) return true;
@@ -84,6 +92,7 @@ export class LinkedList {
   }
 
   find(value) {
+    // Time complexity: O(n)
     let current = this.headNode;
     let index = 0;
     while (current) {
@@ -95,6 +104,7 @@ export class LinkedList {
   }
 
   toString() {
+    // Time complexity: O(n)
     let result = "";
     let current = this.headNode;
     while (current) {
@@ -105,6 +115,7 @@ export class LinkedList {
   }
 
   insertAt(value, index) {
+    // Time complexity: O(n)
     if (index === 0) {
       this.prepend(value);
       return;
@@ -116,6 +127,7 @@ export class LinkedList {
   }
 
   removeAt(index) {
+    // Time complexity: O(n)
     if (index === 0 && this.headNode) {
       this.headNode = this.headNode.nextNode;
       return;
